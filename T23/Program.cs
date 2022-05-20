99b0b22ba79b665d04ae01812570dfab5730e6d3
Anton Stepin <ooosg@rambler.ru>
2022-05-20 09:58:59 +0300

    T23

---
+++ T23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 4, 9.
+// 5 -> 1, 8, 27, 64, 125
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void CubeTable(int FromNummber, int UpToNumber)
+{
+    for (int i = FromNummber; i <= UpToNumber; i++)
+    {
+        int sum = i * i * i;
+        Console.WriteLine($"{i}^3 = {sum}");
+    }
+}
+Console.Write("Введите число от: ");
+int FromNummber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число до: ");
+int UpToNumber = Convert.ToInt32(Console.ReadLine());
+CubeTable(FromNummber, UpToNumber);
